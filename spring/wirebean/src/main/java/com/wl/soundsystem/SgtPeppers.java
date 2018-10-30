dee9e7e26ef12eb2be945b0074ab6d655b8de54c
wangl <394590459@qq.com>
2018-10-30 15:56:11 +0800

    修改

--- spring/wirebean/src/main/java/com/wl/soundsystem/SgtPeppers.java
+++ spring/wirebean/src/main/java/com/wl/soundsystem/SgtPeppers.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Component;
  */
 @Component
 public class SgtPeppers implements CompactDisc {
-    private String title = "Sgt. Pepper's Lonely Hearts Club Band";
+    private String title = "Sgt. Pepper's Lonely Hearts Club Band   ";
     private String artist = "The Beatles";
     @Override
     public void play() {
